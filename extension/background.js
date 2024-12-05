609fc4828d6aa02366689254a1601e43f8aac3fb
SBC - DongLT <donglt@bsscommerce.com>
2024-12-05 15:21:33 +0700

    update

--- extension/background.js
+++ extension/background.js
@@ -14,6 +14,10 @@ function updateRedirectRules() {
             { url: `${domain}/bss-b2b-rf-js.js`, urlFilter: `https://cdn.shopify.com/extensions/*/assets/bss-b2b-rf-js.js` },
             { url: `${domain}/bss-b2b-js.js`, urlFilter: `https://cdn.shopify.com/extensions/*/assets/bss-b2b-js.js` },
             { url: `${domain}/bss-b2b-v3.js`, urlFilter: `https://cdn.shopify.com/extensions/*/assets/bss-b2b-v3.js` },
+            // { url: `https://dev-subscriptions-api-21.dev-bsscommerce.com/bss-sub-form.js`, urlFilter: `https://cdn.shopify.com/extensions/*/assets/bss-sub-form.js` },
+            // { url: `https://dev-subscriptions-api-21.dev-bsscommerce.com/bss-sub.js`, urlFilter: `https://cdn.shopify.com/extensions/*/assets/bss-sub.js` },
+            { url: `https://dev-subscriptions-api-21.dev-bsscommerce.com/bss-sub-com.js`, urlFilter: `https://cdn.shopify.com/extensions/*/assets/bss-sub-com.js` },
+            { url: `https://dev-subscriptions-api-21.dev-bsscommerce.com/bss-sub-v2.js`, urlFilter: `https://cdn.shopify.com/extensions/*/assets/bss-sub-v2.js` },
         ];
 
         const mdRules = modules.map((md, index) => ({
